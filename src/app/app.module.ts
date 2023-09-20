d8d311c5cd2fcb7b4a09451c9723c22bf01b5125
Antonio <50128507+swedishdelight@users.noreply.github.com>
2023-09-20 13:48:17 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RemesasComponent } from './components/remesas/remesas.component';
